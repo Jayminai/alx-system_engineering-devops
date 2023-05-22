915ffa403b64071893eacdefdca752552db465f9
Jayminai <georgegodstime62@gmail.com>
2023-05-22 15:13:21 +0100

    API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,38 +1,31 @@
 #!/usr/bin/python3
-"""REST API for todo lists of employees"""
+"""Accessing a REST API for todo lists of employees"""
 
 import requests
 import sys
 
 
-def fetch_employee_todo_progress(employee_id):
-    base_url = "https://jsonplaceholder.typicode.com"
-    user_url = f"{base_url}/users/{employee_id}"
-    todo_url = f"{base_url}/users/{employee_id}/todos"
+if __name__ == '__main__':
+    employeeId = sys.argv[1]
+    baseUrl = "https://jsonplaceholder.typicode.com/users"
+    url = baseUrl + "/" + employeeId
 
-    try:
-        user_response = requests.get(user_url)
-        todo_response = requests.get(todo_url)
-        user_response.raise_for_status()
-        todo_response.raise_for_status()
-    except requests.exceptions.RequestException as err:
-        print(f"Error occurred: {err}")
-        sys.exit(1)
+    response = requests.get(url)
+    employeeName = response.json().get('name')
 
-    employee_name = user_response.json().get("name")
-    tasks = todo_response.json()
-    completed_tasks = [task for task in tasks if task.get("completed")]
+    todoUrl = url + "/todos"
+    response = requests.get(todoUrl)
+    tasks = response.json()
+    done = 0
+    done_tasks = []
 
-    print(f"Employee {employee_name} is done with tasks({len(completed_tasks)}/{len(tasks)}):")
+    for task in tasks:
+        if task.get('completed'):
+            done_tasks.append(task)
+            done += 1
 
-    for task in completed_tasks:
-        print(f"\t{task.get('title')}")
+    print("Employee {} is done with tasks({}/{}):"
+          .format(employeeName, done, len(tasks)))
 
-
-if __name__ == "__main__":
-    if len(sys.argv) != 2 or not sys.argv[1].isdigit():
-        print("Please provide a valid employee ID as a parameter.")
-        sys.exit(1)
-
-    employee_id = sys.argv[1]
-    fetch_employee_todo_progress(employee_id)
+    for task in done_tasks:
+        print("\t {}".format(task.get('title')))
